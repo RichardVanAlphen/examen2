99b945545bc04541d3df18503baad18c17b385f6
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2025-06-16 17:14:41 +0200

    Test request 3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.ajax({
     url: '../html/blogArtiekelen.html',
     success: function(data){
-        $('#inhoud').append(data)
+        $('#inhoud').append(data, {'aantal' : 3})
     },
     error: function(){
         alert('Er is iets fout gegaan. Probeer later opnieuw alstublieft.');
