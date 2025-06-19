fe23f30254c8bfd598ab8b1e9290b904b58eea46
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2025-06-19 14:46:42 +0200

    Update script.js
    removed old ajax
    added on click test

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    /*$.ajax({
-    url: '../html/blogArtiekelen.html',
-    success: function(data){
-        $('#inhoud').append(data)
-    },
-    error: function(){
-        alert('Er is iets fout gegaan. Probeer later opnieuw alstublieft.');
-    }
-    });*/
     $('#inhoud').load('../html/blogArtiekelen.html .preview');
+
+    $('#article1').on('click', function(){
+        console.log('click1');
+    });
+    $('#article2').on('click', function(){
+        console.log('click2');
+    });
+    $('#article3').on('click', function(){
+        console.log('click3');
+    });
 });
