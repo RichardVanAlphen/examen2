e95aa64e9b9ebf083ad5c4079094fa1ddb73c6ed
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2025-06-19 15:45:31 +0200

    Update script.js
    added load callback to hide the full versions of the articles
    added full article slideToggle

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
-    $('#inhoud').load('../html/blogArtiekelen.html .preview');
+    // Laden artiekelen
+    $('#inhoud').load('../html/blogArtiekelen.html .preview', function(){
+        $('#inhoud .fullversion').hide();
+    });
 
+    // Animaties
+    // Artiekel openen/sluiten
     $('#inhoud').on('click', '#cursus', function(){
+        $('#cursusFullVersion').slideToggle();
         $('#aanmelding').slideToggle();
         $('#docent').slideToggle();
     });
     $('#inhoud').on('click', '#aanmelding', function(){
+        $('#aanmeldingFullVersion').slideToggle();
         $('#cursus').slideToggle();
         $('#docent').slideToggle();
     });
     $('#inhoud').on('click', '#docent', function(){
+        $('#docentFullVersion').slideToggle();
         $('#cursus').slideToggle();
         $('#aanmelding').slideToggle();
     });
